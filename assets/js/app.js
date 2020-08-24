6fab0144a220a500ee5b20c461719d448dc0e466
luislpzr <lpzr97@gmail.com>
2020-08-24 15:59:06 -0500

    Se agrega el cambio de vista

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,21 @@
 const body = document.querySelector('body')
 body.addEventListener('click', showClick)
+localStorage.setItem('tweets', {})
 
 function showClick(e){
      const target = e.target
      if(target.type == 'submit'){
-          console.log('Esto es un botón')
+          addTweet()
+     }
+}
+
+function addTweet(){
+     let text_area = document.getElementById('tweet')
+     const tweet = text_area.value
+     if(tweet.length > 0){
+
      }
      else{
-          console.log('Esto no es un botón')
+          alert('No puedes mandar un Tweet vacío')
      }
 }
